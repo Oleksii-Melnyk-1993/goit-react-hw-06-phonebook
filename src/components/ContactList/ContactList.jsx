e21f30c1908d1e78346ed08fd4777bb5cd42fa35
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-08-26 20:19:25 +0300

    add log

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,6 +6,7 @@ import { getContacts, getFilter } from 'redux/selectors';
 export const ContactList = () => {
   const filteredContacts = useSelector(getFilter);
   const contacts = useSelector(getContacts);
+  console.log(filteredContacts);
 
   const getFilteredContacts = () => {
     const normalizedFilter = filteredContacts.toLowerCase();
